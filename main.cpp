5e86d9fc2d77a57f78b619f7786821493e87db81
Alex-f98 <bfuentes@fi.uba.ar>
2025-05-08 16:34:28 +0000

    9a

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+#define TIME_DELAY_MS  10
 
+//################# DigitalIn - C #############################################
 typedef struct {
     gpio_t gpio;
 } DigitalIn_t;
@@ -11,10 +13,6 @@ static inline void DigitalIn_init(DigitalIn_t *obj, PinName pin) {
     gpio_init_in(&obj->gpio, pin);
 }
 
-//static inline void DigitalIn_init_ex(DigitalIn_t *obj, PinName pin, PinMode mode) {
-//    gpio_init_in_ex(&obj->gpio, pin, mode);
-//}
-
 static inline int DigitalIn_read(DigitalIn_t *obj) {
     return gpio_read(&obj->gpio);
 }
@@ -23,9 +21,28 @@ static inline void DigitalIn_mode(DigitalIn_t *obj, PinMode mode) {
     gpio_mode(&obj->gpio, mode);
 }
 
-//static inline int DigitalIn_is_connected(DigitalIn_t *obj) {
-//    return gpio_is_connected(&obj->gpio);
-//}
+
+//################# DigitalOut - C #############################################
+typedef struct {
+    gpio_t gpio;
+} DigitalOut_t;
+
+//static==privado, inline== remplaza la funcion por su contenido.
+static inline void DigitaOut_init(DigitalOut_t *obj, PinName pin) {
+    gpio_init_out(&obj->gpio, pin);
+}
+
+static inline void DigitalOut_write(DigitalOut_t * obj, int value){
+    // Thread safe / atomic HAL call
+    gpio_write(&obj->gpio, value);
+}
+
+static inline int DigitalOut_read(DigitalOut_t *obj) {
+    return gpio_read(&obj->gpio);
+}
+
+
+//##################################################
 
 int main() {
     // Entradas
@@ -33,33 +50,44 @@ int main() {
     DigitalIn_t overTempDetector;
     DigitalIn_t alarmOffButton;
 
-    DigitalIn_init(&gasDetector, D2);
+    DigitalIn_init(&gasDetector     , D2);
+    DigitalIn_init(&overTempDetector, D3);
+    DigitalIn_init(&alarmOffButton  , BUTTON1);
+
+    DigitalIn_mode(&overTempDetector, PullDown);
     DigitalIn_mode(&gasDetector, PullDown);
 
-    DigitalIn_init(&overTempDetector, D3);
-    DigitalIn_mode(&overTempDetector, PullDown);
 
-    DigitalIn_init(&alarmOffButton, BUTTON1);
+    DigitalOut_t alarmLed;
+    DigitaOut_init(&alarmLed, LED1);
 
-
-    DigitalOut alarmLed(LED1);  //C++
     int alarmState = OFF;
+    int gas = 0, temp=0, btn=0; 
+    int timeDebug = 0;
+    while (true) {
+        // Leer entradas
+        int gas  = DigitalIn_read(&gasDetector);
+        int temp = DigitalIn_read(&overTempDetector);
+        int btn  = DigitalIn_read(&alarmOffButton);
 
-    while (1) {
-        if (DigitalIn_read(&gasDetector) || DigitalIn_read(&overTempDetector)) {
+        // Lógica de activación
+        if (gas || temp)
             alarmState = ON;
-            printf("Gas detectado: %s ; alarmLed: %s\n", gasDetector ? "true" : "false", alarmLed ? "true" : "false");
 
-        }
-        alarmLed = alarmState;
-        if (DigitalIn_read(&alarmOffButton)) {
+        DigitalOut_write(&alarmLed, alarmState);
+
+        if (btn) 
             alarmState = OFF;
-            printf("Gas detectado: %s ; alarmLed: %s\n", gasDetector ? "true" : "false", alarmLed ? "true" : "false");
 
+        timeDebug += TIME_DELAY_MS;
+        if (timeDebug > 500){
+            printf("Gas: %d, Temp: %d, Boton: %d, LED: %d\n",
+               gas, temp, btn, DigitalOut_read(&alarmLed));
         }
+
+        delay(TIME_DELAY_MS);
     }
 
-    return 0;
 }
 
 
